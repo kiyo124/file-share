5eda3f7234154d0970674f653fa5241dc7f17680
kiyo124 <112853824+kiyo124@users.noreply.github.com>
2024-07-03 11:55:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ DB_NAME = os.environ.get("DATABASE_NAME", "luffybot1)")
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002190278598"))
 
-TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "0"))
+TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
