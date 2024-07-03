913686b0065dd8586671d548dd796d12852bb186
kiyo124 <112853824+kiyo124@users.noreply.github.com>
2024-07-03 10:23:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,8 +28,8 @@ OWNER_ID = int(os.environ.get("1871953626", "1871953626"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://luffybot01:<Ilovehack529@#>@cluster0.raxwipv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = os.environ.get("DATABASE_NAME", "luffybot01)")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://luffbot1:<password>@cluster0.raxwipv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DATABASE_NAME", "luffybot1)")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "2"))
